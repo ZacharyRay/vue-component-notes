7ae06f4640e106eee50d6d4b5cd4ee38b219badd
ZacharyRay <zacharyaagesen@gmail.com>
2020-08-20 17:53:03 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <app-server></app-server>
+</div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      status: 'critical'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/ServerStatus.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div>server status: {{ status }}</div>
+        <button @click="changeStatus">Change Status</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function(){
+            return {
+                status: 'cretical'
+            }
+        },
+        methods: {
+            changeStatus() {
+                this.status = 'Normal';
+            }
+        }
+    }
+</script>
